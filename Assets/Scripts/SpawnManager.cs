26f9d8e61cadc79be5a460a41362e9e1db979da5
Meggie Stenback <ms42524@srcs.loc>
2022-04-12 15:32:05 -0700

    Tutorial 2.4.1-2.4.5

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,15 +7,24 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
 
-    void Update() {
-     if (Input.GetKeyDown(KeyCode.S))
-        {
-            //Randomly generate animal index and spawn position
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], spawnPos,
-            animalPrefabs[animalIndex].transform.rotation);
-        }
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+    }
+
+    void Update() 
+    {
+   
+    }
+
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnpos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+       Instantiate(animalPrefabs[animalIndex], spawnpos, 
+           animalPrefabs[animalIndex].transform.rotation);
     }
 }
